628aad282dbb0177399de70e0e9c7c37f39a701c
Orange-position0 <13963804+orangegithub@user.noreply.gitee.com>
2026-02-24 23:02:59 +0800

    feat: add yazi config files

---
+++ dot_config/yazi/plugins_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# 安装常用插件(install plugins of yazi)
+ya pack -a yazi-rs/plugins:smart-enter
+ya pack -a yazi-rs/plugins:git
+
+# 安装 flavors theme
+ya pkg add yazi-rs/flavors:catppuccin-mocha
+
